9594f4dfbb19faa5506a89375da9edc1839c7daf
Manuel Alejandro de Brito Fontes <aledbf@gmail.com>
2021-08-31 16:34:32 -0400

    Fix typescript upgrade errors

--- components/dashboard/src/App.tsx
+++ components/dashboard/src/App.tsx
@@ -148,7 +148,7 @@ function App() {
     window.addEventListener("hashchange", () => {
         // Refresh on hash change if the path is '/' (new context URL)
         if (window.location.pathname === '/') {
-            window.location.reload(true);
+            window.location.reload();
         }
     }, false);
 
